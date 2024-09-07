ce42c8667a18e196ce98d9f62f1a4e9e77ad26ee
Alex Goliansky <a_goliansky@viyar.ua>
2024-09-07 15:19:13 +0300

    Step 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
-import { Heading } from 'components';
+import { Header } from 'components';
+import { lazy, Suspense, useEffect } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { getUserInfo } from 'service/opencagedataApi';
+
+const Home = lazy(() => import('pages/Home'));
+const Rates = lazy(() => import('pages/Rates'));
 
 export const App = () => {
-  return <Heading title="Just do it!" />;
+  useEffect(() => {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    function success(pos) {
+      getUserInfo(pos.coords);
+    }
+
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Suspense fallback={<div>Loading..</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/rates" element={<Rates />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Suspense>
+    </>
+  );
 };
